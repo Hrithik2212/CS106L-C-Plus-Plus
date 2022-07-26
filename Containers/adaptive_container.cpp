32ca9a5fc8109d33018961d0cf520958086744c1
Hrithik2212 <hr6466@srmist.edu.in>
2022-07-26 20:50:51 +0530

    stack and queue

---
+++ Containers/adaptive_container.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// Stack 
+/* Is a vector with functionality that allows
+ ->push_back
+ ->pop_back
+*/
+// Queue 
+/* A vector with functionallity that allows 
+  ->push_back
+  ->pop front 
+*/
+int main(){
+    
+    return 0;
+}
