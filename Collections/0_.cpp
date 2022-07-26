73587432d43627024636c99346a1faddbb77e32b
Hrithik2212 <hr6466@srmist.edu.in>
2022-07-26 20:51:06 +0530

    ignore

---
+++ Collections/0_.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+/* Collection is an object that stores Data
+   aka Data Structures
+   *Objects that are stored in are called elements 
+*/
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
