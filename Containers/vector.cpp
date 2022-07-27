0e4e7dd2a93cf97337b4f3f29b7938b3472942e3
Hrithik2212 <hr6466@srmist.edu.in>
2022-07-27 11:27:40 +0530

    cpp arrays

---
+++ Containers/vector.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std ;
+
+void printVector(vector<void> vec){
+    for(auto element: vec){
+        cout << element << " , " << endl;
+    }
+}
+
+int main(){
+    vector<int> vec;
+    // vec.add(10);
+    // vec.add(20);
+    // vec.add(30);
+
+    return 0;
+}
